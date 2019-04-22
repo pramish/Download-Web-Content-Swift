be86b92732c8d94a2253c903313708971241f624
Pramish Luitel <luitelpramish7@gmail.com>
2019-04-22 21:15:14 +1000

    Initial Commit

---
+++ DownloadWebContent/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DownloadWebContent
+//
+//  Created by Pramish Luitel on 22/4/19.
+//  Copyright © 2019 Pramish Luitel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
